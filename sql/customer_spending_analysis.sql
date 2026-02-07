3f5fe7be0540ddf2886f7fc43b1c12780c592bb6
YuriiKorol <korolik1995nice@gmail.com>
2026-02-07 10:56:08 +0100

    Enhance SQL query with comments for better understanding
    Updated SQL query to include comments for clarity on calculations and logic.

--- sql/customer_spending_analysis.sql
+++ sql/customer_spending_analysis.sql
@@ -7,22 +7,22 @@ SELECT people.name AS p_name
       ,categories.name AS cat_name
       ,SUM(price)  AS tot_pers_in_cat
 FROM orders
-JOIN shop ON orders.shopId = shop.id
-JOIN people ON orders.personId = people.id           
-JOIN categories ON categories.id = shop.category_id
+JOIN shop ON orders.shopId = shop.id                                          
+JOIN people ON orders.personId = people.id                       -- Calculation of each customer’s spending by category
+JOIN categories ON categories.id = shop.category_id                                   
 GROUP BY people.id, categories.id
 ORDER BY people.id)
 ,
 ranked AS (
 SELECT cat_name
-	  ,avg(tot_pers_in_cat) AS avg_in_category
+	  ,avg(tot_pers_in_cat) AS avg_in_category             -- Average order value by category
 FROM cte
 GROUP BY cat_name)
 SELECT cte.p_name
       ,cte.cat_name
       ,cte.tot_pers_in_cat
       ,ranked.avg_in_category
-	  ,ROUND(((tot_pers_in_cat - avg_in_category) / avg_in_category) * 100, 2) AS pct_diff_from_category_avg
+	  ,ROUND(((tot_pers_in_cat - avg_in_category) / avg_in_category) * 100, 2) AS pct_diff_from_category_avg                -- For each customer, calculate the percentage difference between their spending																															-- and the category average; keep only customers who spent above the category averag
 FROM cte
 JOIN ranked ON ranked.cat_name = cte.cat_name  
 WHERE cte.tot_pers_in_cat > ranked.avg_in_category;
